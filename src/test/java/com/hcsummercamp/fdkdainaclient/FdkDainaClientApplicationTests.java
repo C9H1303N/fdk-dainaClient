8b52b941e489a2357b4777e2c64dcd3c654b873c
yzj <915038446@qq.com>
2021-07-16 19:28:39 +0800

    1

---
+++ src/test/java/com/hcsummercamp/fdkdainaclient/FdkDainaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcsummercamp.fdkdainaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdkDainaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
